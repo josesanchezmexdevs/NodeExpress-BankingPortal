19214cdca3419ebbee5ea3183a24bb7f236a6e68
Jose Sanchez <jose.sanchez@mexmax-internet.com>
2019-05-09 16:44:49 -0500

    module 3

--- src/app.js
+++ src/app.js
@@ -7,11 +7,25 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use( express.static(path.join(__dirname, 'public')));
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'UTF8' });
+const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
-    const title = 'Index';
+    const title = 'Account Summary';
 
     res.render('index', {title: title});
+});
+
+app.get('/savings', (req, res) => {
+    const account = accounts.savings;
+
+    res.render('account', { account: account });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users });
 });
 
 app.listen(3000, () => {
