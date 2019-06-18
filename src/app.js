301cdd63f2d5ae6302a4ac699fe84acb2c3c6aeb
Jose Sanchez <jose.sanchez@mexmax-internet.com>
2019-06-18 14:02:15 -0500

    6.14

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use("/account", accountRoutes);
+app.use("/services", servicesRoutes);
 
 app.get('/', (req, res) => {
     const title = 'Account Summary';
