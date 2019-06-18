da837d1d96e5959e610e61d51ada3aa74af55a5f
Jose Sanchez <jose.sanchez@mexmax-internet.com>
2019-06-18 13:32:28 -0500

    6.9 and 6.10

--- src/app.js
+++ src/app.js
@@ -20,30 +20,6 @@ app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] });
 });
 
-app.get('/transfer', (req, res) =>  {
-    res.render('transfer', {});
-});
-
-app.post('/transfer', (req, res) =>  {
-    const bodyParams = req.body;
-    accounts[bodyParams.from].balance = accounts[bodyParams.from].balance - bodyParams.amount;
-    accounts[bodyParams.to].balance = parseInt(accounts[bodyParams.to].balance) + parseInt(bodyParams.amount, 10);
-    writeJSON();
-    res.render('transfer', { message: "Transfer Completed" });
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', { account: accounts.credit});
-});
-
-app.post('/payment', (req, res) => {
-    const bodyParams = req.body;
-    accounts.credit.balance = accounts.credit.balance - bodyParams.amount;
-    accounts.credit.available = parseInt(bodyParams.amount) + parseInt(accounts.credit.available);
-    writeJSON();
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
-});
-
 app.listen(3000, () => {
     console.log("Project Running on port 3000!");
 });
--- src/routes/services.js
+++ src/routes/services.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const { accounts, writeJSON } = require('../data');
+
+app.get('/transfer', (req, res) =>  {
+    res.render('transfer', {});
+});
+
+app.post('/transfer', (req, res) =>  {
+    const bodyParams = req.body;
+    accounts[bodyParams.from].balance = accounts[bodyParams.from].balance - bodyParams.amount;
+    accounts[bodyParams.to].balance = parseInt(accounts[bodyParams.to].balance) + parseInt(bodyParams.amount, 10);
+    writeJSON();
+    res.render('transfer', { message: "Transfer Completed" });
+});
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit});
+});
+
+app.post('/payment', (req, res) => {
+    const bodyParams = req.body;
+    accounts.credit.balance = accounts.credit.balance - bodyParams.amount;
+    accounts.credit.available = parseInt(bodyParams.amount) + parseInt(accounts.credit.available);
+    writeJSON();
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+});
+
+module.exports = router;
