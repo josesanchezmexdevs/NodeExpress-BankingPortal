d2f5a31912a7669087c3b0bf290b84029cb4ffda
Jose Sanchez <jose.sanchez@mexmax-internet.com>
2019-06-18 13:42:10 -0500

    6.11

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const accountRoutes = require('./routes/accounts');
 
 const { accounts, users, writeJSON } = require('./data');
 
